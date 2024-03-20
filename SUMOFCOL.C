c9c5aa4ca38d4cfac6b353e54194c2a3c2a6701d
heelpatel84 <153573743+heelpatel84@users.noreply.github.com>
2024-03-20 19:06:01 -0400

    Sum_Of_Column

---
+++ SUMOFCOL.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[10][10],i,j,sum,r,c;
+ clrscr();
+ printf("Enter Number of Rows");
+ scanf("%d%d",&i,&j);
+ printf("Enter the value\t");
+ for(r=0;r<i;r++)
+ {
+   for(c=0;c<j;c++)
+   {
+     scanf("%d",&a[r][c]);
+   }
+ }
+ for(r=0;r<i;r++)
+ {
+  sum=0;
+  for(c=0;c<j;c++)
+  {
+   sum = sum + a[c][r];
+  }
+  printf("The Sum of Number is = %d\n",sum);
+ }
+ getch();
+}
+
