37afe4dfbfbffe0dce7bda8cf6f31bbace75f599
heelpatel84 <153573743+heelpatel84@users.noreply.github.com>
2024-03-20 19:11:44 -0400

    Trangle

---
+++ Trangle.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a=180;
+   clrscr();
+   printf("Enter the value");
+   scanf("%d",&a);
+   a==180? printf("Triangle is valid"):printf("Triangle is not valid");
+   getch();
+}
+
